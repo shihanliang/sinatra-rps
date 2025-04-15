99df44f689bded2a6a9152ee5bc977dc54b28d3d
shihanliang <sliang1@ChicagoBooth.edu>
2025-04-15 03:52:34 +0000

    coding

--- app.rb
+++ app.rb
@@ -1,9 +1,34 @@
 require "sinatra"
 require "sinatra/reloader"
 
+def computer_move
+  ["rock", "paper", "scissors"].sample
+end
+
+def outcome(player, computer)
+  return "tied" if player == computer
+
+  rules = {
+    "rock" => "scissors",
+    "paper" => "rock",
+    "scissors" => "paper"
+  }
+
+  rules[player] == computer ? "won" : "lost"
+end
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb :home
+end
+
+["rock", "paper", "scissors"].each do |move|
+  get("/#{move}") do
+    @player_move = move
+    @comp_move = computer_move
+    @result = outcome(@player_move, @comp_move)
+
+    @summary = "We played #{@player_move.capitalize}. They played #{@comp_move.capitalize}. We #{@result}!"
+
+    erb :play
+  end
 end
